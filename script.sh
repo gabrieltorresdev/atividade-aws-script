c1c2c1afc4dff28a6e99fccf764759521c4d9578
Gabriel Torres <86476476+gabrieltorresdev@users.noreply.github.com>
2025-02-24 19:05:43 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Definição do arquivo de log
+LOG_FILE="/var/log/scriptzin.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
+
 # Instalando e ativando Apache
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd
 
-# removendo arquivos da pasta de destino
+# Removendo arquivos da pasta de destino
 sudo rm -rf /var/www/html/*
 
-# clonando projeto web aleatorio
+# Clonando projeto web aleatório
 sudo git clone https://github.com/matheusmanuel/site-simples-com-html-e-css- /var/www/html
 
-# fazendo post para gravar resultado
+# Fazendo post para gravar resultado
 sudo curl -X POST -d "nome=Gabriel Torres" https://difusaotech.com.br/lab/aws/index.php
